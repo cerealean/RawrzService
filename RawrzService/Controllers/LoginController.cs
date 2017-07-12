79717840dfd388831f5649bdee4de52903588ac4
Michael Crawford <cerealean@me.com>
2017-07-12 08:27:16 -0500

    Added login info

---
+++ RawrzService/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+using RawrzService.Models;
+
+namespace RawrzService.Controllers
+{
+    public class LoginController : ApiController
+    {
+        public User Post(Login loginModel)
+        {
+            return new User
+            {
+                Id = 55,
+                FirstName = "Wendy",
+                LastName = "Crawford",
+                Email = "wendy.crawford@fakemail.com",
+                Phone = "5739794672",
+                CanEmail = true,
+                CanText = true
+            };
+        }
+    }
+}
---
+++ RawrzService/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace RawrzService.Models
+{
+    public class User
+    {
+        public int Id;
+        public string FirstName;
+        public string LastName;
+        public string Email;
+        public string Phone;
+        public bool CanText;
+        public bool CanEmail;
+    }
+}
